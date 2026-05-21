32faaacb9c8e1d259218edeac0dc66af211a55ff
hungzazed <phihung0168@gmail.com>
2026-05-21 15:27:33 +0700

    refactor: remove deprecated legacy integration service and associated cleanup logic

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   health(): string {
-    return 'Server is running successfully!';
+    return 'Server is running successfully!!!!!';
   }
 }
